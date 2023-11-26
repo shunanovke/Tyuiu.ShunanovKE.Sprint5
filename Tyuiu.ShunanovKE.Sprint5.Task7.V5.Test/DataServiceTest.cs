6baa63dd0425a19f72effa3eff5a0b9a735eb3ad
shunanovke <shunan05@gmail.com>
2023-11-26 23:45:31 +0500

    Task7

--- Tyuiu.ShunanovKE.Sprint5.Task7.V5.Test/DataServiceTest.cs
+++ Tyuiu.ShunanovKE.Sprint5.Task7.V5.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.ShunanovKE.Sprint5.Task7.V5.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Дарья\source\repos\Tyuiu.ShunanovKE.Sprint5\Tyuiu.ShunanovKE.Sprint5.Task2.V27\bin\Debug\OutPutFileTask7V5.csv";
+            string path = @"C:\Users\Дарья\source\repos\Tyuiu.ShunanovKE.Sprint5\Tyuiu.ShunanovKE.Sprint5.Task7.V5\bin\Debug\OutPutFileTask7V5.txt";
             FileInfo fileinfo = new FileInfo(path);
             bool res = fileinfo.Exists;
             bool wait = true;
